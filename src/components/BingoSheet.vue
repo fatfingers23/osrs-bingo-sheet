106d9b75af4eee80568fd062ef2bd079a32ccfa0
Bailey Townsend <baileytownsend2323@gmail.com>
2023-03-25 12:26:46 -0500

    lower case and escape modifier

--- src/components/BingoSheet.vue
+++ src/components/BingoSheet.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { reactive, computed } from 'vue'
+import {reactive, computed} from 'vue'
 import bingoSheet from '../bingoSheet.json'
 
 interface LooseObject {
@@ -18,35 +18,39 @@ const state = reactive({
   tile: {
     tileName: "",
     description: "",
-    picName:""
+    picName: ""
   },
   search: ''
 });
 
-function openModal(item: LooseObject):void{
+
+document.addEventListener('keyup', function (evt) {
+  if (evt.key === 'Escape') {
+    closeModal()
+  }
+});
+
+function openModal(item: LooseObject): void {
   state.modal = true;
   state.tile.tileName = item.tileName;
   state.tile.description = item.description;
   state.tile.picName = item.picName
 }
 
-function closeModal():void{
+function closeModal(): void {
   state.modal = false;
   state.tile = {
     tileName: "",
     description: "",
-    picName:""
+    picName: ""
   };
 }
 
 const filterList = computed(() => {
-  if(state.search === ''){
-    console.log("test")
-
+  if (state.search === '') {
     return bingoSheet
-  }else{
-    console.log("test")
-    return bingoSheet.filter(tile => tile.tileName.includes(state.search))
+  } else {
+    return bingoSheet.filter(tile => tile.tileName.toLowerCase().includes(state.search.toLowerCase()))
   }
 })
 
@@ -55,36 +59,39 @@ const filterList = computed(() => {
 
 <template>
   <h1>Mega Millions March 2023 Bingo</h1>
-  <sub>  This may be the ugliest quickest site i ever made so plz 4give
+  <sub> This may be the ugliest quickest site i ever made so plz 4give
   </sub>
   <h3>Extra Info</h3>
   <ul style="list-style: none">
     <li>Can be dupe gwd items including nex to cover both tiles</li>
-    <li>When posting the screenshots can you please for sets post say 1/4 or 1/6 for metal boots so its easier for us to track. Example on dragon set you get the med helm put 'dragon set 1/4'
+    <li>When posting the screenshots can you please for sets post say 1/4 or 1/6 for metal boots so its easier for us to
+      track. Example on dragon set you get the med helm put 'dragon set 1/4'
     </li>
   </ul>
-  <input type="text" placeholder="Search..." v-model="state.search"/>
-  <button v-on:click="state.search = ''" >Clear</button>
+  <div style="padding-bottom: 10px">
+    <input type="text" placeholder="Search..." v-model="state.search"/>
+    <button v-on:click="state.search = ''">Clear</button>
+  </div>
   <div class="bingo-board">
     <div v-for="(item, index) in filterList" :key="index">
-    <div class="bingo-tile" v-on:click="openModal(item)" >
-      <!--This code sucks i know but yolo? -->
-      <img :src="`./tiles/${item.picName}.png`">
+      <div class="bingo-tile" v-on:click="openModal(item)">
+        <!--This code sucks i know but yolo? -->
+        <img :src="`./tiles/${item.picName}.png`">
 
-      <!--      <img v-if="index === 0" :src="`./tiles/${index + 1}.png`">-->
-<!--      <img v-else-if="index === 71" :src="`./tiles/${index - 1}.png`">-->
-<!--      <img v-else :src="`./tiles/${index + 1}.png`">-->
-    </div>
+        <!--      <img v-if="index === 0" :src="`./tiles/${index + 1}.png`">-->
+        <!--      <img v-else-if="index === 71" :src="`./tiles/${index - 1}.png`">-->
+        <!--      <img v-else :src="`./tiles/${index + 1}.png`">-->
+      </div>
     </div>
 
     <!-- The Modal -->
-    <div id="myModal" class="modal" :style="{'display': state.modal ? 'block' : 'none'}"  >
+    <div id="myModal" class="modal" :style="{'display': state.modal ? 'block' : 'none'}">
 
       <!-- Modal content -->
       <div class="modal-content">
         <span class="close" v-on:click="closeModal">&times;</span>
-        <p>{{state.tile.tileName}}</p>
-        <p>{{state.tile.description}}</p>
+        <p>{{ state.tile.tileName }}</p>
+        <p>{{ state.tile.description }}</p>
       </div>
 
     </div>
@@ -103,8 +110,8 @@ const filterList = computed(() => {
   width: 100%; /* Full width */
   height: 100%; /* Full height */
   overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0,0,0); /* Fallback color */
-  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+  background-color: rgb(0, 0, 0); /* Fallback color */
+  background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
 }
 
 /* Modal Content/Box */
