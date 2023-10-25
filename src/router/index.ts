b058d336f95172d37605b3029368ef2aa71616e9
Bailey Townsend <baileytownsend2323@gmail.com>
2023-10-24 21:23:31 -0500

    clan vs clan

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import BingoSheet from '../components/BingoSheet.vue';
+import BingoSheet from '../views/BingoSheet.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,14 +14,6 @@ const router = createRouter({
       name: 'team',
       component: BingoSheet
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
   ]
 })
 
