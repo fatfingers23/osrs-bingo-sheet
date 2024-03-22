cfa18ac18dd23d2e3b71bc784690797eb3441cd6
Bailey Townsend <baileytownsend2323@gmail.com>
2024-03-21 23:41:51 -0500

    It's the final count down

--- src/views/BingoSheet.vue
+++ src/views/BingoSheet.vue
@@ -117,8 +117,50 @@ onMounted(async () => {
 })
 
 
-const start = new Date("2024-02-16T18:00:00Z")
-const end = new Date("2024-02-26T00:00:00Z")
+const start = new Date("2024-03-29T18:00:00Z")
+const end = new Date("2024-04-07T00:00:00Z")
+
+
+const _second = 1000;
+const _minute = _second * 60;
+const _hour = _minute * 60;
+const _day = _hour * 24;
+// @ts-ignore
+let timer: Timeout;
+
+const countDown = reactive({
+  days: 0,
+  hours: 0,
+  minutes: 0,
+  seconds: 0,
+  bingoIsGoing: false
+});
+
+const showRemaining = () => {
+  let now = new Date();
+  let endTimeToUse: Date;
+  if (now > start){
+    countDown.bingoIsGoing = true;
+    endTimeToUse = end;
+  }else{
+    endTimeToUse = start;
+  }
+  // @ts-ignore
+  let distance = endTimeToUse - now;
+  if (distance < 0) {
+
+    clearInterval(timer);
+    return;
+  }
+  countDown.days = Math.floor(distance / _day);
+  countDown.hours = Math.floor((distance % _day) / _hour);
+  countDown.minutes = Math.floor((distance % _hour) / _minute);
+  countDown.seconds = Math.floor((distance % _minute) / _second);
+
+}
+
+timer = setInterval(showRemaining, 1000);
+
 </script>
 
 <template>
@@ -129,6 +171,20 @@ const end = new Date("2024-02-26T00:00:00Z")
     <div class="text-center mt-10">
       <h1 class="text-3xl">Insomniacs B-I-N-G-O</h1>
       <span class="text-1xl font-bold">{{ start.toLocaleString() }} till {{ end.toLocaleString() }}</span>
+    </div>
+    <div class="flex justify-center">
+      <div class="flex flex-col">
+        <div class="flex justify-center">
+          <div class="flex flex-row">
+            <div v-show="countDown.days != 0" class="pr-1 text-accent">{{ countDown.days }} days</div>
+            <div v-show="countDown.hours != 0" class="pr-1 text-accent">{{ countDown.hours }} hours</div>
+            <div v-show="countDown.minutes != 0" class="pr-1 text-accent">{{ countDown.minutes }} minutes</div>
+            <div v-show="countDown.seconds != 0" class="pr-1 text-accent">{{ countDown.seconds }} seconds</div>
+            <div v-if="countDown.bingoIsGoing">Till bingo ends</div>
+            <div v-else>Till bingo starts</div>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="text-center mt-3 mb-2">
       <h1 class="text-3xl" v-show="state.teamName != ''">Team: {{ state.teamName }}</h1>
@@ -149,7 +205,7 @@ const end = new Date("2024-02-26T00:00:00Z")
     <!--      </p>-->
     <!--    </div>-->
     <div class="flex justify-center">
-      <div class="form-control ">
+      <div class="form-control">
         <div class="input-group ">
           <input type="text" placeholder="Search…" class="input input-bordered " v-model="state.search"/>
           <button class="btn btn-square" v-on:click="state.search = ''">
@@ -167,7 +223,7 @@ const end = new Date("2024-02-26T00:00:00Z")
           </div>
 
           <div v-else class="cursor-pointer text-center" v-on:click="openModal(item)">
-            <img :src="`./tiles/${item.picName}.png?forcedupdate=1`" class="object-contain max-w-full rounded-lg ">
+            <img :src="`./tiles/${item.picName}.png?forcedupdate=2`" class="object-contain max-w-full rounded-lg ">
             <span v-if="item.portionCompleted !== '0'" class="tile-text ">{{ item.portionCompleted }}</span>
           </div>
         </div>
